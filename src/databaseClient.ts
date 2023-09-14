a4c30548e39b816bbe978aecfbf91f5d983e3e94
henrypgill <henrygill3@gmail.com>
2023-09-14 11:52:48 +0100

    typing for fileQuery to accept response type

--- src/databaseClient.ts
+++ src/databaseClient.ts
@@ -11,7 +11,7 @@ export class DatabaseClient extends Client {
     this.filePath = checkValidFilePath(queryDirectory) ? queryDirectory : "";
   }
 
-  async fileQuery<T extends number | string>(path: string, values?: T[]) {
+  async fileQuery<S, T extends (number | string)[]>(path: string, values?: T) {
     try {
       const query = new FileQuery(`./${this.filePath}/${path}.sql`);
       if (values) {
@@ -19,10 +19,10 @@ export class DatabaseClient extends Client {
           throw new Error(
             `incorrect number of values passed to query: ${query.path}, received ${values.length} values but expected ${query.valuesCount} values.`
           );
-        const response = await this.query(query.queryString, values);
+        const response = await this.query<S, T>(query.queryString, values);
         return response
       } else {
-        const response = await this.query(query.queryString);
+        const response = await this.query<S, T>(query.queryString);
         return response
       }
     } catch (error) {
